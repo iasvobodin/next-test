2c2a0d5ef74cd96fd5c9ed03cae28e1cceaf722b
Ivan <51612499+iasvobodin@users.noreply.github.com>
2023-11-21 05:25:37 +0000

    add photo

--- app/photoseries/[route]/page.tsx
+++ app/photoseries/[route]/page.tsx
@@ -1,6 +1,13 @@
 import prisma from '@/lib/prisma'
 import { getPhotos } from '@/utils/getPhoto'
 
+
+
+
+const photoSrc = (width:string, name:string) =>`https://img.svobodinaphoto.ru/${width}_${name}.webp`
+
+
+
 export async function generateStaticParams() {
   const photoSeries = await prisma.photoSeries.findMany({
     where: {
@@ -13,14 +20,17 @@ export async function generateStaticParams() {
   }))
 }
 
+
+
 export default async function Page({ params }: { params: { route: string } }) {
   // const data = await getData()
   const { route } = params
-  const item = await getPhotos(route)
+  const item = (await getPhotos(route)).map(e => (<img src={photoSrc("320", e.name)} alt="sv" />) )
   return (
     <main>
       <h1>{route}</h1>
-      <p>wow{JSON.stringify(item.map(e => e.name))}</p>
+<div>{item}</div>
+      {/* <p>wow{JSON.stringify(item)}</p> */}
     </main>
 
   )
