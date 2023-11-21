42a38138b6edf8107aca18db561c3f522ca4b554
Ivan <51612499+iasvobodin@users.noreply.github.com>
2023-11-21 05:34:25 +0000

    key

--- app/photoseries/[route]/page.tsx
+++ app/photoseries/[route]/page.tsx
@@ -25,7 +25,7 @@ export async function generateStaticParams() {
 export default async function Page({ params }: { params: { route: string } }) {
   // const data = await getData()
   const { route } = params
-  const item = (await getPhotos(route)).map(e => (<img src={photoSrc("320", e.name)} alt="sv" />) )
+  const item = (await getPhotos(route)).map(e => (<img key={e.id} src={photoSrc("320", e.name)} alt="sv" />) )
   return (
     <main>
       <h1>{route}</h1>
